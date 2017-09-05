765c9e6c3b02bd1cd6cc53d8bc452c2f7d9d021b
David Hale and Sarah Lindmar <david@>
2017-09-05 15:29:50 -0700

    meet all leet specs

--- lib/l33t_speak.rb
+++ lib/l33t_speak.rb
@@ -4,18 +4,28 @@ class Leet
   end
 
   def speak()
-    x = 0
-    @word.split(//)
+    x = -1
+    words = Array.new(@word.split(' '))
 
-    while(x < @word.length)
-      x += 1
-      if (@word[x] === 'e')
-        @word[x] = '3'
-      end
-      if  (@word[x] === 'o')
-        @word[x] = '0'
+    while(x < words.length)
+    x += 1
+    y = 0
+      while(y < words[x].to_s.length)
+        if (words[x][y] === 'e' || words[x][y] === 'E')
+          words[x][y] = '3'
+        end
+        if (words[x][y] === 'o' || words[x][y] === 'O')
+          words[x][y] = '0'
+        end
+        if (words[x][y] === 'I')
+          words[x][y] = '1'
+        end
+        y += 1
+        if (words[x][y] === 's' || words[x][y] === 'S')
+          words[x][y] = '$'
+        end
       end
     end
-    @word
+    words.join(' ')
   end
 end
--- spec/l33t_speak_spec.rb
+++ spec/l33t_speak_spec.rb
@@ -3,14 +3,29 @@ require('rspec')
 require('l33t_speak')
 
 describe('#speak') do
-  it('replaces all e\'s with 3\'s') do
-      ex1 = Leet.new('hello')
-      expect(ex1.speak).to(eq("h3ll0"))
-  end
+  it('replaces all e\'s with 3\'s and o\'s with 0\'s') do
+    example = Leet.new('hello')
+    expect(example.speak).to(eq("h3ll0"))
+end
 
-  it('replaces all o\'s with 0\'s') do
-      ex2 = Leet.new('hello')
-      expect(ex2.speak).to(eq("h3ll0"))
+it('replaces all E\'s with 3\'s and all O\'s with 0\'s') do
+    example = Leet.new('EpOc')
+    expect(example.speak).to(eq("3p0c"))
+end
+
+it('replaces all I\'s with 1\'s') do
+    example = Leet.new('Ick')
+    expect(example.speak).to(eq("1ck"))
+end
+
+it('replaces all s\'s with $\'s') do
+    example = Leet.new('Signs')
+    expect(example.speak).to(eq("Sign$"))
+end
+
+it('replaces letters correctly in a string of words') do
+    example = Leet.new('I scream you scream we all scream for raspberry ice cream.')
+    expect(example.speak).to(eq('1 scr3am y0u scr3am w3 all scr3am f0r ra$pb3rry ic3 cr3am.'))
   end
 
 end
